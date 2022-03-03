b5a4a6fe27ef569e0b0f4dc1da3236e9b55c5f12
wictormagnusson <wictor.magnusson@outlook.com>
2022-03-03 14:49:39 +0100

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using System.IO;
+using System.Security.Cryptography;
+using System;
+using System.Collections.Generic;
+   
+   class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Writeline("Hejhej");
+            Console.ReadKey();
+        }
+    }
+}
